f6e942ff2b341713cdb4f430e85189985977983a
damho <156370887+Damdev12@users.noreply.github.com>
2025-01-10 08:39:13 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,36 @@
+basic.showString("Hi")
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # # . # #
+    # # . # #
+    `)
+basic.showLeds(`
+    # # . # .
+    . . . . .
+    . . # . .
+    # . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . # # .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    . . . . .
+    . . . # .
+    # # # # #
+    . . . # .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
