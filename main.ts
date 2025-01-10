17c80ca8a85839bdf3f8e9c01c97b100a73e90af
damho <156370887+Damdev12@users.noreply.github.com>
2025-01-10 09:37:05 +0100

    neuer Code

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    music.play(music.createSoundExpression(WaveShape.Noise, 54, 54, 255, 255, 5000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    basic.showString("Damian ")
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # . # #
+        # . . . #
+        . . . . .
+        # . . . #
+        # # . # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Szygula")
+    basic.showIcon(IconNames.Sad)
+})
 basic.showString("Hi")
 basic.showLeds(`
     # . . . .
@@ -7,29 +46,11 @@ basic.showLeds(`
     # # . # #
     `)
 basic.showLeds(`
-    # # . # .
-    . . . . .
     . . # . .
-    # . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    # # # . .
-    # . # # .
-    # # # . #
-    # . # . #
-    # . # . #
-    `)
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.Heart)
-basic.showLeds(`
-    . . . . .
-    . . . # .
+    . # # # .
     # # # # #
-    . . . # .
-    . . . . .
+    . # # # .
+    . . # . .
     `)
 basic.forever(function () {
 	
